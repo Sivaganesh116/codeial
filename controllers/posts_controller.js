0be5db959544d0e2388c8f6bc79113cfc1509106
Sivaganesh116 <sivaganesh7785@gmail.com>
2023-07-05 10:22:37 +0530

    create router and controller for creating posts and store the data in database

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,16 @@
-module.exports.posts = function(req, res){
-    res.end('<h1> Posts </h1>');
+const Post = require('../models/post');
+
+module.exports.create = function(req, res){
+    Post.create({
+        content: req.body.content,
+        user: req.user._id
+    })
+    .then(post=>{
+        console.log("New Post:")
+        console.log(post);
+        return res.redirect('back');
+    })
+    .catch(err=>{
+        console.log("Error creating post: ", err);
+    })
 }
