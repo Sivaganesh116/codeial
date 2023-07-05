619a5368460093751bbc17baebed911dbd9fe0af
Sivaganesh116 <sivaganesh7785@gmail.com>
2023-07-05 11:10:36 +0530

    create posts list section in home and populated it with posts in the db with the user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,17 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    return res.render('home', {
-        title: 'Home'
-    });
+
+    Post.find({})
+    .populate('user') //populate the user of each post
+    .then(posts=>{
+        return res.render('home', {
+            title: 'codeial | Home',
+            posts: posts
+        });
+    })
+    .catch(err=>{
+        console.log("Error finding posts: ", err);
+    })
 }
 
